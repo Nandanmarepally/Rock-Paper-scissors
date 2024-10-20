212dba6f988d168d53c530dc1da3e1e1134859f7
Nandan <nandanvmarepally@gmail.com>
2024-10-20 14:11:53 +0000

    Rock-Paper-scissors

---
+++ src/Game/styledComponents.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  background-color: #223a5f;
+  height: 100vh;
+  padding: 30px;
+  display: flex;
+  justfify-content: space-between;
+`
+
+export const ScoreContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justfify-content: space-between;
+`
+
+export const ItemsContainer = styled.div``
+
+export const Heading = styled.h1`
+  font-size: 20px;
+`
+
+export const ScoreCardContainer = styled.div`
+  background-color: #ffffff;
+`
+
+export const ParagraphScore = styled.p`
+  text-align: center;
+  margin: 0;
+`
+
+export const ScoreSpan = styled.p`
+  color: #000000;
+`
+
+export const ItemsImagesContainer = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 100%;
+  height: 50%;
+`
+
+export const PlayAgainButton = styled.button``
+
+export const PopUpContainer = styled.div``
+
+export const PopUpButton = styled(PlayAgainButton)``
+
+export const RulesImageContainer = styled.div``
+
+export const RulesImage = styled.img``
+
+export const CloseLineButton = styled.button``
+
+export const CloseLineContainer = styled.div``
